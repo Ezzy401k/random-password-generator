41acc0febd3c38c22a22c6ea551b3f6f553fc59f
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-24 22:12:53 +0300

    Update superior password generator.py

--- superior password generator.py
+++ superior password generator.py
@@ -1,12 +1,13 @@
-#Password Generator Project
+# Password Generator Project
 import random
+
 # List the characters that will be needed.
 lower_letters = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
-upper_letters = [ 'A', 'B', 'C', 'D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
-                 'M', 'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+upper_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -15,10 +16,10 @@ print("Welcome to the PyPassword Generator!")
 use = input("If you want a random password type 'y' or for a custom password length type 'n':\n")
 # If they want a random password execute this.
 if use == "y":
-   lr_letters = random.randint(3,7)
-   up_letters = random.randint(2,5)
-   nr_numbers = random.randint(2,5)
-   nr_symbols = random.randint(1,4)
+    lr_letters = random.randint(3, 7)
+    up_letters = random.randint(2, 5)
+    nr_numbers = random.randint(2, 5)
+    nr_symbols = random.randint(1, 4)
 # If the want a custom password run this.
 elif use == "n":
     lr_letters = int(input("How many lower case letters would you like in your password?\n"))
@@ -33,20 +34,20 @@ code3 = ''
 code4 = ''
 # Cycle through the characters and choose if "y" or "n" is inputted..
 if use == "y" or use == "n":
-    for i in range(0,up_letters):
+    for i in range(0, up_letters):
         temp = random.choice(lower_letters)
         code1 += temp
-    for i in range(0,lr_letters):
+    for i in range(0, lr_letters):
         temp1 = random.choice(upper_letters)
         code2 += temp1
-    for j in range(0,nr_symbols):
+    for j in range(0, nr_symbols):
         temp2 = random.choice(symbols)
         code3 += temp2
-    for k in range(0,nr_numbers):
+    for k in range(0, nr_numbers):
         temp3 = random.choice(numbers)
         code4 += temp3
     # Convert the strings to a list after contacting them.
-    list_code = list(code1 + code2 + code3+code4)
+    list_code = list(code1 + code2 + code3 + code4)
     # shuffle through the list 3 times because I like it.
     random.shuffle(list_code)
     random.shuffle(list_code)
@@ -54,11 +55,10 @@ if use == "y" or use == "n":
     # Convert the list to a string that gets displayed to the user.
     shuffled_passcode = ''
     for n in list_code:
-      shuffled_passcode += n
+        shuffled_passcode += n
     print(shuffled_passcode)
-    
+
 # If a wrong input is given.
 else:
-  print("Please Enter a valid input.")
+    print("Please Enter a valid input.")
 input("Tap ENTER to EXIT!")
-
